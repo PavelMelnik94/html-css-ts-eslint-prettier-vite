e4a1404fe55bfdc968b7fc31fb58c1df02784631
Pavel Melnik <recyclesordie94@gmail.com>
2024-12-10 23:35:00 +0300

    add rotation navigation

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,7 @@
 import { $ } from './_lib';
 
 document.addEventListener('DOMContentLoaded', () => {
-	// code there
+	$('[data-button="open"]').on('click', () => $('.container').addClass('show-nav'));
+
+	$('[data-button="close"]').on('click', () => $('.container').removeClass('show-nav'));
 });

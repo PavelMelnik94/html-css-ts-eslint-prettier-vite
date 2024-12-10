fc6bd13937f6bdd8edabe189cecff8088873e265
Pavel <p.melnik@intelico.su>
2024-12-10 10:54:56 +0300

    add expanding-cards

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,4 +4,11 @@ import { initTheme, toggleTheme } from './theme';
 document.addEventListener('DOMContentLoaded', () => {
 	initTheme();
 	$('[data-button="toggle-theme"]').on('click', toggleTheme);
+
+	const removeActiveClass = () => $('.panel').removeClass('active');
+
+	$('.panel').on('click', (e: Event) => {
+		removeActiveClass();
+		if (e.currentTarget) (e.currentTarget as HTMLElement).classList.add('active');
+	});
 });

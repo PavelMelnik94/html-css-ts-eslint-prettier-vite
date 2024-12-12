4ea9fb4e0cb6d94b28c4d9242dca64055285e8e3
Pavel Melnik <recyclesordie94@gmail.com>
2024-12-12 03:09:14 +0300

    fixes

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,5 +3,6 @@ import { initTheme, toggleTheme } from './theme';
 
 document.addEventListener('DOMContentLoaded', () => {
 	initTheme();
+
 	$('[data-button="toggle-theme"]').on('click', toggleTheme);
 });

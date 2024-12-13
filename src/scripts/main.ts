c86b3494207826a6a116fec2e4923ea0b4c686a6
Pavel <p.melnik@intelico.su>
2024-12-13 17:27:36 +0300

    add blurry-loading

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,16 @@
+import {$} from "./_lib.ts";
+
 document.addEventListener('DOMContentLoaded', () => {
-	// code there
+
+	let load = 0;
+
+	let int = setInterval(blurring, 30);
+	function blurring() {
+		load++;
+		if (load > 99) {
+			clearInterval(int);
+		}
+		$('.loading-text').setText(`${load}%`).css({opacity: `${1 - load / 100}`})
+		$('.bg').css({filter: `blur(${100 - load}px)`});
+	}
 });

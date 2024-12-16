61aa7014aafcc05b77c03a69eedca92b5f3b7254
PavelMelnik <p.melnik@intelico.su>
2024-12-16 17:33:32 +0300

    add form wave label

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,1 @@
-import { $ } from 'dom-utils-minimize';
-document.addEventListener('DOMContentLoaded', () => {
-	// eslint-disable-next-line no-console
-	$('.button').on('click', (_e) => console.log('Клик!'));
-});
+document.addEventListener('DOMContentLoaded', () => {});

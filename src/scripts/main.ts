18f6aca7820827c3251a97dea0412b50ab0115b7
Pavel <p.melnik@intelico.su>
2024-12-16 10:47:31 +0300

    add debug console

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,6 @@
 import { $ } from 'dom-utils-minimize';
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+	// eslint-disable-next-line no-console
+	console.log(`'DOM is ready ${$}'`);
+});
